8e5322fd0826a2406b0a02618f6c21c587fb5a12
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-04-04 09:28:24 -0300

    EacRubyUtils::Envs::BaseEnv.

---
+++ lib/eac_ruby_utils/envs/base_env.rb
@@ -0,0 +1,13 @@
+module EacRubyUtils
+  module Envs
+    class BaseEnv
+      def command(*args)
+        ::EacRubyUtils::Envs::Command.new(self, args)
+      end
+
+      def file_exist?(file)
+        command(['stat', file]).execute[:exit_code].zero?
+      end
+    end
+  end
+end
