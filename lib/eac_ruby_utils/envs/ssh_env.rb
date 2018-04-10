15796e99e6b50511978aa5d0c66448f75fefaea4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-04-10 16:47:13 -0300

    Fix EacLauncher::Envs::SshEnv namespace.

--- lib/eac_ruby_utils/envs/ssh_env.rb
+++ lib/eac_ruby_utils/envs/ssh_env.rb
@@ -1,15 +1,17 @@
-module Envs
-  class SshEnv < ::EacRubyUtils::Envs::BaseEnv
-    def initialize(user_hostname)
-      @user_hostname = user_hostname
-    end
+module EacRubyUtils
+  module Envs
+    class SshEnv < ::EacRubyUtils::Envs::BaseEnv
+      def initialize(user_hostname)
+        @user_hostname = user_hostname
+      end
 
-    def to_s
-      "SSH(#{@user_hostname})"
-    end
+      def to_s
+        "SSH(#{@user_hostname})"
+      end
 
-    def command_line(line)
-      "ssh #{Shellwords.escape(@user_hostname)} #{Shellwords.escape(line)}"
+      def command_line(line)
+        "ssh #{Shellwords.escape(@user_hostname)} #{Shellwords.escape(line)}"
+      end
     end
   end
 end
