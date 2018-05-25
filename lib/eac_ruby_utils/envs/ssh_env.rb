e284a260dbb8dac9e757d6dd8875615e52d5c01f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-05-25 19:13:33 -0300

    Ruby/Redmine: opções em URLs SSH.

--- lib/eac_ruby_utils/envs/ssh_env.rb
+++ lib/eac_ruby_utils/envs/ssh_env.rb
@@ -1,16 +1,51 @@
 module EacRubyUtils
   module Envs
     class SshEnv < ::EacRubyUtils::Envs::BaseEnv
-      def initialize(user_hostname)
-        @user_hostname = user_hostname
+      class << self
+        def parse_uri(uri)
+          r = parse_user_hostname(uri) || ::Addressable::URI.parse(uri)
+          return r if r.scheme == 'ssh'
+          raise "URI has no SSH scheme: #{uri}"
+        end
+
+        private
+
+        def parse_user_hostname(s)
+          m = /\A([^@]+)@([^@]+)\z/.match(s)
+          m ? ::Addressable::URI.new(scheme: 'ssh', host: m[2], user: m[1]) : nil
+        rescue Addressable::URI::InvalidURIError
+          nil
+        end
+      end
+
+      def initialize(uri)
+        @uri = self.class.parse_uri(uri)
       end
 
       def to_s
-        "SSH(#{@user_hostname})"
+        uri.to_s
       end
 
       def command_line(line)
-        "ssh #{Shellwords.escape(@user_hostname)} #{Shellwords.escape(line)}"
+        "#{ssh_command_line} #{Shellwords.escape(line)}"
+      end
+
+      private
+
+      attr_reader :uri
+
+      def ssh_command_line
+        r = %w[ssh]
+        r += ['-p', uri.port] if uri.port.present?
+        r += ssh_command_line_options
+        r << "#{uri.user}@#{uri.host}"
+        r.map { |a| Shellwords.escape(a) }.join(' ')
+      end
+
+      def ssh_command_line_options
+        r = []
+        uri.query_values.each { |k, v| r += ['-o', "#{k}=#{v}"] } if uri.query_values
+        r
       end
     end
   end
