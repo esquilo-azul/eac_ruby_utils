9423c9233471b58b55144cf05917193fdfa4eaf5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-07-11 15:59:32 -0300

    EacRubyUtils::ArgumentsConsumer.

--- lib/eac_ruby_utils.rb
+++ lib/eac_ruby_utils.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module EacRubyUtils
+  require 'eac_ruby_utils/arguments_consumer'
   require 'eac_ruby_utils/console'
   require 'eac_ruby_utils/envs'
   require 'eac_ruby_utils/options_consumer'
