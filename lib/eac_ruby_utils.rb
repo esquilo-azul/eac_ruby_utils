300033f804c71ae752b49113af818e86abcd03bb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-07-15 14:39:31 -0300

    Hash.to_sym_keys_hash.

--- lib/eac_ruby_utils.rb
+++ lib/eac_ruby_utils.rb
@@ -5,5 +5,6 @@ module EacRubyUtils
   require 'eac_ruby_utils/console'
   require 'eac_ruby_utils/envs'
   require 'eac_ruby_utils/options_consumer'
+  require 'eac_ruby_utils/patches'
   require 'eac_ruby_utils/yaml'
 end
