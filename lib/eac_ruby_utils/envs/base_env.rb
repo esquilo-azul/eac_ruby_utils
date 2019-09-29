6760ae2d2d76ce3416274d0b7847aaf64cf4fd32
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-09-29 19:22:18 -0300

    EacRubyUtils::Envs::BaseEnv.executable.

--- lib/eac_ruby_utils/envs/base_env.rb
+++ lib/eac_ruby_utils/envs/base_env.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/envs/command'
+require 'eac_ruby_utils/envs/executable'
 
 module EacRubyUtils
   module Envs
@@ -11,6 +12,10 @@ module EacRubyUtils
 
       def file_exist?(file)
         command(['stat', file]).execute[:exit_code].zero?
+      end
+
+      def executable(*executable_new_args)
+        ::EacRubyUtils::Envs::Executable.new(self, *executable_new_args)
       end
     end
   end
