9cf2eae286d7b9485a58c5635cfabf10d635bccb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-10-02 16:19:36 -0300

    EacRubyUtils::Envs::BaseEnv.file.

--- lib/eac_ruby_utils/envs/base_env.rb
+++ lib/eac_ruby_utils/envs/base_env.rb
@@ -2,6 +2,7 @@
 
 require 'eac_ruby_utils/envs/command'
 require 'eac_ruby_utils/envs/executable'
+require 'eac_ruby_utils/envs/file'
 
 module EacRubyUtils
   module Envs
@@ -10,12 +11,17 @@ module EacRubyUtils
         ::EacRubyUtils::Envs::Command.new(self, args)
       end
 
+      # <b>DEPRECATED:</b> Please use <tt>file(file).exist?</tt> instead.
       def file_exist?(file)
-        command(['stat', file]).execute[:exit_code].zero?
+        self.file(file).exist?
       end
 
       def executable(*executable_new_args)
         ::EacRubyUtils::Envs::Executable.new(self, *executable_new_args)
+      end
+
+      def file(path)
+        ::EacRubyUtils::Envs::File.new(self, path)
       end
     end
   end
