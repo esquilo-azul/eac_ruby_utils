37e1bf558d1c413fbc4661c8ed1dd81a1b7fc534
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-14 14:26:48 -0300

    EacRubyUtils::Templates::File.

---
+++ lib/eac_ruby_utils/templates/file.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+require 'active_support/core_ext/hash/indifferent_access'
+require 'eac_ruby_utils/simple_cache'
+
+module EacRubyUtils
+  module Templates
+    class File
+      include ::EacRubyUtils::SimpleCache
+
+      VARIABLE_DELIMITER = ::Regexp.quote('%%')
+      VARIABLE_PATTERN = /#{VARIABLE_DELIMITER}([a-z0-9\._]*)#{VARIABLE_DELIMITER}/i.freeze
+
+      attr_reader :path
+
+      def initialize(path)
+        @path = path
+      end
+
+      # +variables_provider+ A [Hash] or object which responds to +read_entry(entry_name)+.
+      def apply(variables_source)
+        variables_provider = build_variables_provider(variables_source)
+        variables.inject(content) do |a, e|
+          a.gsub(variable_pattern(e), variables_provider.variable_value(e).to_s)
+        end
+      end
+
+      def apply_to_file(variables_source, output_file_path)
+        ::File.write(output_file_path, apply(variables_source))
+      end
+
+      private
+
+      def variables_uncached
+        content.scan(VARIABLE_PATTERN).map(&:first).map do |name|
+          sanitize_variable_name(name)
+        end.to_set
+      end
+
+      def content_uncached
+        ::File.read(path)
+      end
+
+      def sanitize_variable_name(variable_name)
+        variable_name.to_s.downcase
+      end
+
+      def build_variables_provider(variables_source)
+        return HashVariablesProvider.new(variables_source) if variables_source.is_a?(::Hash)
+        return EntriesReaderVariablesProvider.new(variables_source) if
+        variables_source.respond_to?(:read_entry)
+
+        raise "Variables provider not found for #{variables_source}"
+      end
+
+      def variable_pattern(name)
+        /#{VARIABLE_DELIMITER}#{::Regexp.quote(name)}#{VARIABLE_DELIMITER}/i
+      end
+
+      class BaseVariablesProvider
+        attr_reader :source
+
+        def initialize(source)
+          @source = source
+        end
+
+        def variable_value(name)
+          return variable_fetch(name) if variable_exist?(name)
+
+          raise VariableNotFoundError, "Variable \"#{name}\" not found in #{source}"
+        end
+      end
+
+      class HashVariablesProvider < BaseVariablesProvider
+        def initialize(source)
+          super(source.with_indifferent_access)
+        end
+
+        def variable_exist?(name)
+          source.key?(name)
+        end
+
+        def variable_fetch(name)
+          source.fetch(name)
+        end
+      end
+
+      class EntriesReaderVariablesProvider < BaseVariablesProvider
+        def variable_exist?(_name)
+          true
+        end
+
+        def variable_fetch(name)
+          source.read_entry(name)
+        end
+      end
+
+      class VariableNotFoundError < StandardError; end
+    end
+  end
+end
---
+++ spec/lib/eac_ruby_utils/templates/file_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/templates/file'
+
+RSpec.describe ::EacRubyUtils::Templates::File do
+  let(:files_dir) { ::File.join(__dir__, 'file_spec_files') }
+  let(:source_path) { ::File.join(files_dir, 'source.template') }
+  let(:instance) { described_class.new(source_path) }
+  let(:expected_content) { ::File.read(::File.join(files_dir, 'expected_content')) }
+
+  describe '#apply' do
+    context 'when config is a hash' do
+      let(:config) { { name: 'Fulano de Tal', age: 33 } }
+
+      it { expect(instance.apply(config)).to eq(expected_content) }
+    end
+
+    context 'when config responds to read_entry' do
+      let(:config) { ::EacRubyUtils::Configs.new('rspec_describe_eac_ruby_utils_templates_file') }
+
+      before do
+        config.clear
+        config['name'] = 'Fulano de Tal'
+        config['age'] = '33'
+      end
+
+      it { expect(instance.apply(config)).to eq(expected_content) }
+    end
+  end
+
+  describe '#variables' do
+    it { expect(instance.variables).to eq(::Set.new(%w[name age])) }
+  end
+end
