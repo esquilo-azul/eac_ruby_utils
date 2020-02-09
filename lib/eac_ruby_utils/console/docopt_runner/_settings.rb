462f4d023140afcefb3ed4573cdedf2ca5d83d96
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-02-09 14:04:16 -0300

    EacRubyUtils::Console::DocoptRunner: uses EacRubyUtils::SettingsProvider.

--- lib/eac_ruby_utils/console/docopt_runner/_settings.rb
+++ lib/eac_ruby_utils/console/docopt_runner/_settings.rb
@@ -1,45 +1,18 @@
 # frozen_string_literal: true
 
+require 'eac_ruby_utils/settings_provider'
+
 module EacRubyUtils
   module Console
     class DocoptRunner
+      include ::EacRubyUtils::SettingsProvider
+
       attr_reader :settings
 
       private
 
       def setting_value(key, required = true)
-        %i[setting_from_method setting_from_argument setting_from_constant].each do |method|
-          value = send(method, key)
-          return value if value
-        end
-        return nil unless required
-
-        raise "Setting \"#{key}\" not found. Implement a \"#{key}\" method" \
-          ", declare a #{setting_constant(key, true)} constant" \
-          "or pass a #{key.to_sym} option to #{self.class.name}.new() method."
-      end
-
-      def setting_from_argument(key)
-        settings[key]
-      end
-
-      def setting_from_constant(key)
-        constant_name = setting_constant(key)
-        begin
-          self.class.const_get(constant_name)
-        rescue NameError
-          nil
-        end
-      end
-
-      def setting_from_method(key)
-        respond_to?(key) ? send(key) : nil
-      end
-
-      def setting_constant(key, fullname = false)
-        name = key.to_s.underscore.upcase
-        name = "#{self.class.name}::#{name}" if fullname
-        name
+        super(key, required: required, order: %w[method settings_object constant])
       end
     end
   end
