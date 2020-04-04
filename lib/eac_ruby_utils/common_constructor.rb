8b091538b774e5657fb191c42e9d5035cc8f3188
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-04-04 18:14:51 -0300

    EacRubyUtils::CommonConstructor.setup_class_initialize: refact.

--- lib/eac_ruby_utils/common_constructor.rb
+++ lib/eac_ruby_utils/common_constructor.rb
@@ -27,28 +27,48 @@ module EacRubyUtils
     end
 
     def setup_class_initialize(klass)
+      common_constructor = self
       klass.include(::ActiveSupport::Callbacks)
       klass.define_callbacks :initialize
-      klass.class_eval initialize_method_code, __FILE__, __LINE__
-    end
-
-    def initialize_method_code
-      b = "def initialize(#{initialize_method_args_code})\n"
-      b += "  run_callbacks :initialize do\n"
-      initialize_method_args.each do |arg|
-        b += "  self.#{arg} = #{arg}\n"
+      klass.send(:define_method, :initialize) do |*args|
+        Initialize.new(common_constructor, args, self).run
       end
-      b += "  end\n"
-      b += "end\n"
-      b
     end
 
-    def initialize_method_args_code
-      initialize_method_args.join(', ')
-    end
+    class Initialize
+      attr_reader :common_constructor, :args, :object
 
-    def initialize_method_args
-      args
+      def initialize(common_constructor, args, object)
+        @common_constructor = common_constructor
+        @args = args
+        @object = object
+      end
+
+      def run
+        validate_args_count
+        object.run_callbacks :initialize do
+          object_attributes_set
+        end
+      end
+
+      private
+
+      def arg_value(arg_name)
+        args[common_constructor.args.index(arg_name)]
+      end
+
+      def object_attributes_set
+        common_constructor.args.each do |arg_name|
+          object.send("#{arg_name}=", arg_value(arg_name))
+        end
+      end
+
+      def validate_args_count
+        return if args.count == common_constructor.args.count
+
+        raise ArgumentError, "#{object.class}.initialize: wrong number of arguments" \
+          " (given #{args.count}, expected #{common_constructor.args.count})"
+      end
     end
   end
 end
