d0fc2b724620cb70733c34e10402e4a002e22c2d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-04-25 19:32:48 -0300

    EacRubyUtils: refact.

--- lib/eac_ruby_utils.rb
+++ lib/eac_ruby_utils.rb
@@ -1,24 +1,6 @@
 # frozen_string_literal: true
 
 module EacRubyUtils
-  require 'eac_ruby_utils/arguments_consumer'
-  require 'eac_ruby_utils/by_reference'
-  require 'eac_ruby_utils/configs'
-  require 'eac_ruby_utils/console'
-  require 'eac_ruby_utils/contextualizable'
-  require 'eac_ruby_utils/core_ext'
-  require 'eac_ruby_utils/envs'
-  require 'eac_ruby_utils/filesystem_cache'
-  require 'eac_ruby_utils/filesystem_traverser'
-  require 'eac_ruby_utils/fs_cache'
-  require 'eac_ruby_utils/listable'
-  require 'eac_ruby_utils/options_consumer'
-  require 'eac_ruby_utils/settings_provider'
-  require 'eac_ruby_utils/patch'
-  require 'eac_ruby_utils/patches'
-  require 'eac_ruby_utils/paths_hash'
   require 'eac_ruby_utils/require_sub'
-  require 'eac_ruby_utils/rspec'
-  require 'eac_ruby_utils/simple_cache'
-  require 'eac_ruby_utils/yaml'
+  ::EacRubyUtils.require_sub __FILE__
 end
