d6ac7325133408b2b4ee9ab1680db860d13eb7c5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-04-25 23:48:22 -0300

    Object.common_construct: pass &block to EacRubyUtils::CommonConstructor.

--- lib/eac_ruby_utils/patches/class/common_constructor.rb
+++ lib/eac_ruby_utils/patches/class/common_constructor.rb
@@ -3,7 +3,7 @@
 require 'eac_ruby_utils/common_constructor'
 
 class Class
-  def common_constructor(*args)
-    ::EacRubyUtils::CommonConstructor.new(*args).setup_class(self)
+  def common_constructor(*args, &block)
+    ::EacRubyUtils::CommonConstructor.new(*args, &block).setup_class(self)
   end
 end
