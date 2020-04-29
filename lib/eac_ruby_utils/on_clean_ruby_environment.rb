ea07ce78e4303cc3156cce30d8fd139187919270
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-04-29 11:51:37 -0300

    EacRubyUtils.on_clean_ruby_environment: deprecated by EacRubyUtils::Ruby.on_clean_environment.

--- lib/eac_ruby_utils/on_clean_ruby_environment.rb
+++ lib/eac_ruby_utils/on_clean_ruby_environment.rb
@@ -1,23 +1,12 @@
 # frozen_string_literal: true
 
+require 'eac_ruby_utils/ruby/on_clean_environment'
+
 module EacRubyUtils
   class << self
-    def on_clean_ruby_environment
-      on_clean_envvars('BUNDLE', 'RUBY') { yield }
-    end
-
-    private
-
-    def on_clean_envvars(*start_with_vars)
-      old_values = envvars_starting_with(start_with_vars)
-      old_values.keys.each { |k| ENV.delete(k) }
-      yield
-    ensure
-      old_values&.each { |k, v| ENV[k] = v }
-    end
-
-    def envvars_starting_with(start_with_vars)
-      ENV.select { |k, _v| start_with_vars.any? { |var| k.start_with?(var) } }
+    # <b>DEPRECATED:</b> Please use <tt>EacRubyUtils::Ruby.on_clean_environment</tt> instead.
+    def on_clean_ruby_environment(*args, &block)
+      ::EacRubyUtils::Ruby.on_clean_environment(*args, &block)
     end
   end
 end
