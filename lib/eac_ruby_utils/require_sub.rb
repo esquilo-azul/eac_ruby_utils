5c791f26951ed2b14a45b0fbca9de1d07e4e7c3c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-05-28 22:47:20 -0300

    EacRubyUtils::RequireSub: extracted from EacRubyUtils.require_sub.

--- lib/eac_ruby_utils/require_sub.rb
+++ lib/eac_ruby_utils/require_sub.rb
@@ -3,6 +3,24 @@
 module EacRubyUtils
   class << self
     def require_sub(file)
+      ::EacRubyUtils::RequireSub.new(file).apply
+    end
+  end
+
+  class RequireSub
+    attr_reader :file
+
+    def initialize(file)
+      @file = file
+    end
+
+    def apply
+      require_sub_files
+    end
+
+    private
+
+    def require_sub_files
       Dir["#{File.dirname(file)}/#{::File.basename(file, '.*')}/*.rb"].sort.each do |path|
         require path
       end
