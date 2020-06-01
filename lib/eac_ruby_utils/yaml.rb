dca12d50e88bd38ca5530da8550513b21e6447cf
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-06-01 12:04:47 -0300

    EacRubyUtils::Yaml.permitted_classes: extracted from .load.

--- lib/eac_ruby_utils/yaml.rb
+++ lib/eac_ruby_utils/yaml.rb
@@ -6,8 +6,14 @@ module EacRubyUtils
   # A safe YAML loader/dumper with common types included.
   class Yaml
     class << self
+      DEFAULT_PERMITTED_CLASSES = [::Symbol, ::Date].freeze
+
       def load(string)
-        ::YAML.safe_load(string, [::Symbol, ::Date])
+        ::YAML.safe_load(string, permitted_classes)
+      end
+
+      def permitted_classes
+        DEFAULT_PERMITTED_CLASSES
       end
     end
   end
