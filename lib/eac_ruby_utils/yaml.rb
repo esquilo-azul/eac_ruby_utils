4f0788f8d3c58c472c8edf43ead999e67bcdd92e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-06-01 13:16:28 -0300

    EacRubyUtils::Yaml.sanitize.

--- lib/eac_ruby_utils/yaml.rb
+++ lib/eac_ruby_utils/yaml.rb
@@ -6,7 +6,8 @@ module EacRubyUtils
   # A safe YAML loader/dumper with common types included.
   class Yaml
     class << self
-      DEFAULT_PERMITTED_CLASSES = [::Symbol, ::Date].freeze
+      DEFAULT_PERMITTED_CLASSES = [::Array, ::Date, ::FalseClass, ::Hash, ::NilClass, ::Numeric,
+                                   ::String, ::Symbol, ::Time, ::TrueClass].freeze
 
       def load(string)
         ::YAML.safe_load(string, permitted_classes)
@@ -14,6 +15,67 @@ module EacRubyUtils
 
       def permitted_classes
         DEFAULT_PERMITTED_CLASSES
+      end
+
+      def sanitize(object)
+        Sanitizer.new(object).result
+      end
+
+      class Sanitizer
+        attr_reader :source
+
+        RESULT_TYPES = %w[permitted enumerableable hashable].freeze
+
+        def initialize(source)
+          @source = source
+        end
+
+        def result
+          RESULT_TYPES.each do |type|
+            return send("result_#{type}") if send("result_#{type}?")
+          end
+
+          source.to_s
+        end
+
+        private
+
+        def result_enumerableable?
+          source.respond_to?(:to_a) && !source.is_a?(::Hash)
+        end
+
+        def result_enumerableable
+          source.to_a.map { |child| sanitize_value(child) }
+        end
+
+        def result_hashable?
+          source.respond_to?(:to_h)
+        end
+
+        def result_hashable
+          source.to_h.map { |k, v| [k.to_sym, sanitize_value(v)] }.to_h
+        end
+
+        def result_nil?
+          source.nil?
+        end
+
+        def result_nil
+          nil
+        end
+
+        def result_permitted?
+          (::EacRubyUtils::Yaml.permitted_classes - [::Array, ::Hash])
+            .any? { |klass| source.is_a?(klass) }
+        end
+
+        def result_permitted
+          source
+        end
+
+        def sanitize_value(value)
+          self.class.new(value).result
+        end
       end
     end
   end
---
+++ spec/lib/eac_ruby_utils/yaml_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'active_support/hash_with_indifferent_access'
+require 'eac_ruby_utils/yaml'
+
+RSpec.describe ::EacRubyUtils::Yaml do
+  let(:unpermitted_class) do
+    Class.new do
+      def initialize(value)
+        @value = value
+      end
+
+      def to_s
+        @value.to_s
+      end
+    end
+  end
+
+  let(:arrayliable_class) do
+    Class.new do
+      def initialize(value)
+        @value = value
+      end
+
+      def to_a
+        [@value]
+      end
+    end
+  end
+
+  let(:source) do
+    {
+      a: 'a', b: :b, c: false, d: true, e: nil,
+      f: [
+        'f',
+        {
+          g: 'g',
+          h: ::ActiveSupport::HashWithIndifferentAccess.new(
+            'i' => ['i', unpermitted_class.new('j')],
+            k: arrayliable_class.new('k')
+          )
+        }
+      ]
+    }
+  end
+
+  let(:target) do
+    {
+      a: 'a', b: :b, c: false, d: true, e: nil,
+      f: ['f', { g: 'g', h: { i: %w[i j], k: ['k'] } }]
+    }
+  end
+
+  describe '#sanitize' do
+    it { expect(described_class.sanitize(target)).to eq(target) }
+  end
+end
