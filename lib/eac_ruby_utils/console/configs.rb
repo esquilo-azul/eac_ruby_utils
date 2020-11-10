7f75eabb40552a01096bde801903e18e8e2df052
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-10 13:54:49 -0300

    EacRubyUtils::Console::Configs::ReadEntryOptions: move to own file.

--- lib/eac_ruby_utils/console/configs.rb
+++ lib/eac_ruby_utils/console/configs.rb
@@ -6,6 +6,7 @@ require 'eac_ruby_utils/core_ext'
 module EacRubyUtils
   module Console
     class Configs
+      require_sub __FILE__
       enable_console_speaker
 
       class << self
@@ -96,38 +97,6 @@ module EacRubyUtils
                                     options.request_input_options)
         warn('Entered value is blank') if entry_value.blank?
         entry_value
-      end
-
-      class ReadEntryOptions
-        enable_simple_cache
-        common_constructor :options
-
-        DEFAULT_VALUES = {
-          before_input: nil, bool: false, list: false, noecho: false, noenv: false, noinput: false,
-          required: true, validator: nil
-        }.freeze
-
-        def [](key)
-          values.fetch(key.to_sym)
-        end
-
-        def request_input_options
-          values.slice(:bool, :list, :noecho)
-        end
-
-        private
-
-        def values_uncached
-          consumer = options.to_options_consumer
-          r = {}
-          DEFAULT_VALUES.each do |key, default_value|
-            value = consumer.consume(key)
-            value = default_value if value.nil?
-            r[key] = value
-          end
-          consumer.validate
-          r
-        end
       end
     end
   end
