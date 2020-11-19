026de3769ea73fc3ef3f31b0c6476b82d8208c7b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-19 11:22:55 -0300

    EacRubyUtils::Console::Configs::EntryReader: extract from EacRubyUtils::Console::Configs.

--- lib/eac_ruby_utils/console/configs.rb
+++ lib/eac_ruby_utils/console/configs.rb
@@ -11,13 +11,7 @@ module EacRubyUtils
 
       class << self
         def entry_key_to_envvar_name(entry_key)
-          path = if entry_key.is_a?(::Array)
-                   entry_key
-                 else
-                   ::EacRubyUtils::PathsHash.parse_entry_key(entry_key)
-                 end
-          path.join('_').gsub(/[^a-z0-9_]/i, '').gsub(/\A_+/, '').gsub(/_+\z/, '')
-              .gsub(/_{2,}/, '_').upcase
+          ::EacRubyUtils::Console::Configs::EntryReader.entry_key_to_envvar_name(entry_key)
         end
       end
 
@@ -40,16 +34,7 @@ module EacRubyUtils
       end
 
       def read_entry(entry_key, options = {})
-        options = ReadEntryOptions.new(options)
-        unless options[:noenv]
-          envvar_value = envvar_read_entry(entry_key)
-          return envvar_value if envvar_value.present?
-        end
-        stored_value = configs.read_entry(entry_key)
-        return stored_value if stored_value
-        return read_entry_from_console(entry_key, options) unless options[:noinput]
-
-        raise "No value found for entry \"#{entry_key}\"" if options[:required]
+        ::EacRubyUtils::Console::Configs::EntryReader.new(self, entry_key, options).read
       end
 
       def store_passwords?
@@ -62,41 +47,10 @@ module EacRubyUtils
 
       protected
 
-      def envvar_read_entry(entry_key)
-        env_entry_key = self.class.entry_key_to_envvar_name(entry_key)
-        return nil unless ENV.key?(env_entry_key)
-
-        ENV.fetch(env_entry_key).if_present(::EacRubyUtils::BlankNotBlank.instance)
-      end
-
-      def read_entry_from_console(entry_key, options)
-        options[:before_input].call if options[:before_input].present?
-        entry_value = looped_entry_value_from_input(entry_key, options)
-        configs.write_entry(entry_key, entry_value)
-        entry_value
-      end
-
       def store_password_banner
         infom 'Do you wanna to store passwords?'
         infom "Warning: the passwords will be store in clear text in \"#{configs.storage_path}\""
         infom 'Enter "yes" or "no"'
-      end
-
-      def looped_entry_value_from_input(entry_key, options)
-        loop do
-          entry_value = entry_value_from_input(entry_key, options)
-          next if entry_value.blank?
-          next if options[:validator] && !options[:validator].call(entry_value)
-
-          return entry_value
-        end
-      end
-
-      def entry_value_from_input(entry_key, options)
-        entry_value = request_input("Value for entry \"#{entry_key}\"",
-                                    options.request_input_options)
-        warn('Entered value is blank') if entry_value.blank?
-        entry_value
       end
     end
   end
