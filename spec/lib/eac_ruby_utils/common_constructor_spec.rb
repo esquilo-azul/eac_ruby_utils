87a0319a9e8a2d19109acc61446d9dc2cb5ec0ad
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-12-05 18:07:26 -0300

    RSpec.describe(EacRubyUtils::CommonConstructor): remove from .rubocop.yml's exclusions.

--- spec/lib/eac_ruby_utils/common_constructor_spec.rb
+++ spec/lib/eac_ruby_utils/common_constructor_spec.rb
@@ -3,34 +3,37 @@
 require 'eac_ruby_utils/common_constructor'
 
 RSpec.describe ::EacRubyUtils::CommonConstructor do
-  ARG_LIST = %i[a b c d].freeze
+  ARG_LIST = %i[a b c d].freeze # rubocop:disable RSpec/LeakyConstantDeclaration
+
   let(:instance) do
     described_class.new(*ARG_LIST, default: %w[Vcc Vd]) do
       @z = 'Vz'
     end
   end
 
-  class MyClass
-    attr_reader :z
+  let(:a_class) do
+    ::Class.new do
+      attr_reader :z
+    end
   end
 
-  let(:subject) { MyClass.new('Va', 'Vb', 'Vc') }
+  let(:a_class_instance) { a_class.new('Va', 'Vb', 'Vc') }
 
   before do
-    instance.setup_class(::MyClass)
+    instance.setup_class(a_class)
   end
 
-  it { expect(subject.z).to eq('Vz') }
+  it { expect(a_class_instance.z).to eq('Vz') }
 
   ARG_LIST.each do |attr|
     expected_value = "V#{attr}"
     it "attribute \"#{attr}\" equal to \"#{expected_value}\"" do
-      expect(subject.send(attr)).to eq(expected_value)
+      expect(a_class_instance.send(attr)).to eq(expected_value)
     end
 
     [false, true].each do |include_all|
       it "respond_to?('#{attr}', #{include_all}) == #{include_all}" do
-        expect(subject.respond_to?("#{attr}=", include_all)).to eq(include_all)
+        expect(a_class_instance.respond_to?("#{attr}=", include_all)).to eq(include_all)
       end
     end
   end
