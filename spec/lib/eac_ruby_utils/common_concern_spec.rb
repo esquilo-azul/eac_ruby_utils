af43db111c443c5efc71e336199e10f692572331
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-12-16 00:32:07 -0300

    RSpec.describe(EacRubyUtils::CommonConcern): remove from Rubocop's exclusions.

--- spec/lib/eac_ruby_utils/common_concern_spec.rb
+++ spec/lib/eac_ruby_utils/common_concern_spec.rb
@@ -9,34 +9,38 @@ RSpec.describe ::EacRubyUtils::CommonConcern do
     end
   end
 
-  module MyModule
-    module ClassMethods
-      def my_class_method
-        'class'
+  let(:stub_module) do
+    ::Module.new do
+      module ClassMethods # rubocop:disable RSpec/LeakyConstantDeclaration
+        def my_class_method
+          'class'
+        end
       end
-    end
 
-    module InstanceMethods
-      def my_instance_method
-        'instance'
+      module InstanceMethods # rubocop:disable RSpec/LeakyConstantDeclaration
+        def my_instance_method
+          'instance'
+        end
       end
     end
   end
 
-  class MyClass
-    class << self
-      attr_accessor :valor
+  let(:stub_class) do
+    ::Class.new do
+      class << self
+        attr_accessor :valor
+      end
     end
   end
 
-  let(:subject) { MyClass.new }
+  let(:stub_class_instance) { stub_class.new }
 
   before do
-    instance.setup(MyModule)
-    MyClass.include MyModule
+    instance.setup(stub_module)
+    stub_class.include stub_module
   end
 
-  it { expect(subject.my_instance_method).to eq('instance') }
-  it { expect(subject.class.my_class_method).to eq('class') }
-  it { expect(subject.class.valor).to eq('changed') }
+  it { expect(stub_class_instance.my_instance_method).to eq('instance') }
+  it { expect(stub_class_instance.class.my_class_method).to eq('class') }
+  it { expect(stub_class_instance.class.valor).to eq('changed') }
 end
