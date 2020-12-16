cedf296dc49fb79edfa2515a864f9940297dd307
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-12-16 01:02:08 -0300

    EacRubyUtils::CommonConcern::ClassSetup: extract from EacRubyUtils::CommonConcern::ModuleSetup.

--- lib/eac_ruby_utils/common_concern/module_setup.rb
+++ lib/eac_ruby_utils/common_concern/module_setup.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'active_support/concern'
+require 'eac_ruby_utils/common_concern/class_setup'
 require 'eac_ruby_utils/simple_cache'
 require 'eac_ruby_utils/patches/object/if_present'
 
@@ -19,36 +20,8 @@ module EacRubyUtils
         setup = self
         a_module.extend(::ActiveSupport::Concern)
         a_module.included do
-          %w[class_methods instance_methods after_callback].each do |suffix|
-            setup.send("setup_#{suffix}", self)
-          end
+          ::EacRubyUtils::CommonConcern::ClassSetup.new(setup, self).run
         end
-      end
-
-      def setup_class_methods(base)
-        class_methods_module.if_present { |v| base.extend v }
-      end
-
-      def setup_instance_methods(base)
-        instance_methods_module.if_present { |v| base.include v }
-      end
-
-      def setup_after_callback(base)
-        common_concern.after_callback.if_present do |v|
-          base.instance_eval(&v)
-        end
-      end
-
-      def class_methods_module_uncached
-        a_module.const_get(CLASS_METHODS_MODULE_NAME)
-      rescue NameError
-        nil
-      end
-
-      def instance_methods_module_uncached
-        a_module.const_get(INSTANCE_METHODS_MODULE_NAME)
-      rescue NameError
-        nil
       end
     end
   end
