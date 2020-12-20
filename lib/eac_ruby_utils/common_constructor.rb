6a7659aaa108feb9105db36620ade2ac2347501a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-12-20 12:29:17 -0300

    EacRubyUtils::CommonConstructor::Initialize: move to own file.

--- lib/eac_ruby_utils/common_constructor.rb
+++ lib/eac_ruby_utils/common_constructor.rb
@@ -2,6 +2,7 @@
 
 require 'active_support/callbacks'
 require 'eac_ruby_utils/arguments_consumer'
+require 'eac_ruby_utils/common_constructor/instance_initialize'
 require 'ostruct'
 
 module EacRubyUtils
@@ -80,61 +81,14 @@ module EacRubyUtils
       klass.include(::ActiveSupport::Callbacks)
       klass.define_callbacks :initialize
       klass.send(:define_method, :initialize) do |*args|
-        Initialize.new(common_constructor, args, self).run
+        ::EacRubyUtils::CommonConstructor::InstanceInitialize.new(common_constructor, args, self)
+                                                             .run
         super(*SuperArgs.new(common_constructor, args, self).result)
       end
     end
 
     def super_args
       options[:super_args]
-    end
-
-    class Initialize
-      attr_reader :common_constructor, :args, :object
-
-      def initialize(common_constructor, args, object)
-        @common_constructor = common_constructor
-        @args = args
-        @object = object
-      end
-
-      def run
-        validate_args_count
-        object.run_callbacks :initialize do
-          object_attributes_set
-          object_after_callback
-        end
-      end
-
-      private
-
-      def arg_value(arg_name)
-        arg_index = common_constructor.args.index(arg_name)
-        if arg_index < args.count
-          args[arg_index]
-        else
-          common_constructor.default_values[arg_index - common_constructor.args_count_min]
-        end
-      end
-
-      def object_after_callback
-        return unless common_constructor.after_set_block
-
-        object.instance_eval(&common_constructor.after_set_block)
-      end
-
-      def object_attributes_set
-        common_constructor.args.each do |arg_name|
-          object.send("#{arg_name}=", arg_value(arg_name))
-        end
-      end
-
-      def validate_args_count
-        return if common_constructor.args_count.include?(args.count)
-
-        raise ArgumentError, "#{object.class}.initialize: wrong number of arguments" \
-          " (given #{args.count}, expected #{common_constructor.args_count})"
-      end
     end
 
     class SuperArgs
