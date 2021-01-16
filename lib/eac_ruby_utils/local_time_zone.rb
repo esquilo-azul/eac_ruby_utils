c97b3cdbfa1918e7bbc3e0b4786ea2a128d20a70
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-01-16 15:10:52 -0300

    EacRubyUtils::LocalTimeZone.by_debian_config.

--- lib/eac_ruby_utils/local_time_zone.rb
+++ lib/eac_ruby_utils/local_time_zone.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
 
+require 'active_support/core_ext/object/blank'
 require 'active_support/values/time_zone'
 
 module EacRubyUtils
   module LocalTimeZone
+    DEBIAN_CONFIG_PATH = '/etc/timezone'
+
     class << self
       TIMEDATECTL_TIMEZONE_LINE_PATTERN = %r{\s*Time zone:\s*(\S+/\S+)\s}.freeze
 
       def auto
-        %w[offset].lazy.map { |s| send("by_#{s}") }.find(&:present?)
+        %w[debian_config offset].lazy.map { |s| send("by_#{s}") }.find(&:present?)
+      end
+
+      def by_debian_config
+        path = ::Pathname.new(DEBIAN_CONFIG_PATH)
+        path.exist? ? path.read.strip.presence : nil
       end
 
       def by_offset
