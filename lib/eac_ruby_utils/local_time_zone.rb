23a3471775f471eed5a8b3d4ec5ce57ada216f92
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-01-16 13:52:58 -0300

    EacRubyUtils::LocalTimeZone: move from Time (eac_ruby_utils/patches/time/local_time_zone).

---
+++ lib/eac_ruby_utils/local_time_zone.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'active_support/values/time_zone'
+
+module EacRubyUtils
+  module LocalTimeZone
+    class << self
+      TIMEDATECTL_TIMEZONE_LINE_PATTERN = %r{\s*Time zone:\s*(\S+/\S+)\s}.freeze
+
+      def local_time_zone
+        local_time_zone_by_timedatectl || local_time_zone_by_offset
+      end
+
+      def local_time_zone_by_timedatectl
+        executable = ::EacRubyUtils::Envs.local.executable('timedatectl', '--version')
+        return nil unless executable.exist?
+
+        TIMEDATECTL_TIMEZONE_LINE_PATTERN.if_match(executable.command.execute!) { |m| m[1] }
+      end
+
+      def local_time_zone_by_offset
+        ::ActiveSupport::TimeZone[::Time.now.getlocal.gmt_offset].name
+      end
+    end
+  end
+end
--- lib/eac_ruby_utils/patches/time/default_time_zone_set.rb
+++ lib/eac_ruby_utils/patches/time/default_time_zone_set.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 require 'active_support/core_ext/time/zones'
-require 'eac_ruby_utils/patches/time/local_time_zone'
+require 'eac_ruby_utils/local_time_zone'
 
-::Time.zone = ::Time.local_time_zone
+::Time.zone = ::EacRubyUtils::LocalTimeZone.local_time_zone
