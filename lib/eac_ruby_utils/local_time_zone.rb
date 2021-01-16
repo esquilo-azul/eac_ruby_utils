7e01c350da492f0250e5b2d22a6fe5d4b7601a97
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-01-16 13:59:11 -0300

    EacRubyUtils::LocalTimeZone.by_timedatectl: rename from .local_time_zone_by_timedatectl.

--- lib/eac_ruby_utils/local_time_zone.rb
+++ lib/eac_ruby_utils/local_time_zone.rb
@@ -11,15 +11,15 @@ module EacRubyUtils
         ::ActiveSupport::TimeZone[::Time.now.getlocal.gmt_offset].name
       end
 
-      def local_time_zone
-        local_time_zone_by_timedatectl || by_offset
-      end
-
-      def local_time_zone_by_timedatectl
+      def by_timedatectl
         executable = ::EacRubyUtils::Envs.local.executable('timedatectl', '--version')
         return nil unless executable.exist?
 
         TIMEDATECTL_TIMEZONE_LINE_PATTERN.if_match(executable.command.execute!) { |m| m[1] }
+      end
+
+      def local_time_zone
+        by_timedatectl || by_offset
       end
     end
   end
