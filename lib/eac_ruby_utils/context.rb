99f3f3705c4b17a02829aea93166f91474fed308
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-06-01 16:00:52 -0300

    EacRubyUtils::Context.

---
+++ lib/eac_ruby_utils/context.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module EacRubyUtils
+  class Context
+    def current
+      optional_current || raise('No elements in context')
+    end
+
+    def optional_current
+      stack.last
+    end
+
+    delegate :pop, to: :stack
+    delegate :push, to: :stack
+
+    def on(obj)
+      push(obj)
+      begin
+        yield
+      ensure
+        pop
+      end
+    end
+
+    private
+
+    def stack
+      @stack ||= []
+    end
+  end
+end
