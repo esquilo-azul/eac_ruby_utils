9bef26bb9944ca780e628b223e3b12443f4afcc7
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-07-31 00:08:13 -0300

    EacRubyUtils::Rspec::Setup.

---
+++ lib/eac_ruby_utils/rspec/setup.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module EacRubyUtils
+  module Rspec
+    class Setup
+      require_sub __FILE__
+      common_constructor :setup_obj
+
+      def perform; end
+    end
+  end
+end
