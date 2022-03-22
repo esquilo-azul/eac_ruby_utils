a0f16a804bff3a0c8f7a656e09c4037644fff63d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-03-21 23:47:13 -0300

    Object.assert_count.

--- lib/eac_ruby_utils/patches/object/asserts.rb
+++ lib/eac_ruby_utils/patches/object/asserts.rb
@@ -11,4 +11,15 @@ class Object
           "Argument \"#{argument_name}\" is not a #{klass}" \
           "(Actual class: #{self.class}, actual value: #{self})"
   end
+
+  # Raises a ArgumentError if +self.count+ is not equal to +count+.
+  #
+  # @return +self+
+  def assert_count(count, argument_name = 'unknown_argument_name')
+    return self if self.count == count
+
+    raise ::ArgumentError,
+          "Argument \"#{argument_name}\" has wrong elements count" \
+          "(Actual: #{self.count}, Required: #{count})"
+  end
 end
