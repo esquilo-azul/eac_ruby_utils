26ddead54a3f0cde65747b63960580d75a22a234
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-05-31 19:11:55 -0300

    EacRubyUtils::Listable::Value.translation_required?.

--- lib/eac_ruby_utils/listable/value.rb
+++ lib/eac_ruby_utils/listable/value.rb
@@ -7,10 +7,11 @@ module EacRubyUtils
     class Value
       attr_reader :value, :key
 
-      def initialize(list, value, key)
+      def initialize(list, value, key, translation_required = true)
         @list = list
         @value = value
         @key = key
+        @translation_required = translation_required
       end
 
       def to_s
@@ -29,10 +30,19 @@ module EacRubyUtils
         translate('description')
       end
 
+      def translation_required?
+        @translation_required
+      end
+
       private
 
       def translate(translate_key)
-        ::I18n.t("#{@list.i18n_key}.#{@key}.#{translate_key}")
+        full_translate_key = "#{@list.i18n_key}.#{@key}.#{translate_key}"
+        if !::I18n.exists?(full_translate_key) && !translation_required?
+          ''
+        else
+          ::I18n.t(full_translate_key)
+        end
       end
     end
   end
