4a832a2bbdda742e36fff9eef31a3174be950122
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-06-12 14:07:59 -0300

    EacRubyUtils::Locales::ModuleI18nTranslate: extract from Module.i18n_translate.

--- lib/eac_ruby_utils/patches/module/i18n_translate.rb
+++ lib/eac_ruby_utils/patches/module/i18n_translate.rb
@@ -1,32 +1,9 @@
 # frozen_string_literal: true
 
-require 'active_support/core_ext/string/inflections'
-require 'i18n'
+require 'eac_ruby_utils/locales/module_i18n_translate'
 
 class Module
-  TRANSLATE_LOCALE_KEY = :__locale
-
   def i18n_translate(entry_suffix, values = {})
-    on_i18n_locale(values.delete(TRANSLATE_LOCALE_KEY)) do
-      ::I18n.translate(i18n_translate_entry_full(entry_suffix), values)
-    end
-  end
-
-  def i18n_translate_entry_full(entry_suffix)
-    "#{i18n_translate_entry_self_prefix}.#{entry_suffix}"
-  end
-
-  def i18n_translate_entry_self_prefix
-    name.underscore.gsub('/', '.')
-  end
-
-  def on_i18n_locale(locale)
-    old_locale = ::I18n.locale
-    begin
-      ::I18n.locale = locale
-      yield
-    ensure
-      ::I18n.locale = old_locale
-    end
+    ::EacRubyUtils::Locales::ModuleI18nTranslate.new(self, entry_suffix, values).result
   end
 end
