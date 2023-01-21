7be99196512826550628c1de452ee6e9ecd24b61
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-01-21 14:11:33 -0300

    EacRubyUtils::Immutable::HashAccessor.immutable_value_get_single: extract from .apply_set.

--- lib/eac_ruby_utils/immutable/hash_accessor.rb
+++ lib/eac_ruby_utils/immutable/hash_accessor.rb
@@ -15,6 +15,10 @@ module EacRubyUtils
         super || {}
       end
 
+      def immutable_value_get_single(object, key)
+        immutable_value_get(object, key)
+      end
+
       def immutable_value_set(object, key, value)
         duplicate_object(object) do |old_value|
           (old_value || {}).merge(key => value)
@@ -34,7 +38,7 @@ module EacRubyUtils
         accessor = self
         klass.send(:define_method, name) do |*args|
           case args.count
-          when 1 then next accessor.immutable_value_get(self, args[0])
+          when 1 then next accessor.immutable_value_get_single(self, args[0])
           when 2 then next accessor.immutable_value_set(self, *args[0..1])
           else
             raise ::ArgumentError, "wrong number of arguments (given #{args.count}, expected 1..2)"
