29caf321ac693e8ce8610f6ca9689259d5e5ce48
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-19 00:28:34 -0300

    .rubocop.yml#Rails/I18nLocaleAssignment: remove.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'tmpdir'
 
 require 'i18n'
 ::I18n.load_path << ::File.join(__dir__, 'locales/pt-BR.yml')
-::I18n.locale = 'pt-BR'
+::I18n.locale = 'pt-BR' # rubocop:disable Rails/I18nLocaleAssignment
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = ::File.join(::Dir.tmpdir, 'eac_ruby_utils_rspec')
