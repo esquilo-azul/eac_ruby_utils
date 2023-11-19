4c28f7e1ff90e6fc041846e1f592fbb4321e39ed
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-19 00:29:17 -0300

    .rubocop.yml#Style/ArgumentsForwarding: remove.

--- lib/eac_ruby_utils/patches/class/common_constructor.rb
+++ lib/eac_ruby_utils/patches/class/common_constructor.rb
@@ -3,7 +3,7 @@
 require 'eac_ruby_utils/common_constructor'
 
 class Class
-  def common_constructor(*args, &block)
-    ::EacRubyUtils::CommonConstructor.new(*args, &block).setup_class(self)
+  def common_constructor(...)
+    ::EacRubyUtils::CommonConstructor.new(...).setup_class(self)
   end
 end
