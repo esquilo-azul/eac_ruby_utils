ea870d349a2814760b7ced711b7918f36d1977fd
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-19 00:08:44 -0300

    .rubocop.yml#Layout/SpaceAroundMethodCallOperator: remove.

--- spec/lib/eac_ruby_utils/patches/object/if_present_spec.rb
+++ spec/lib/eac_ruby_utils/patches/object/if_present_spec.rb
@@ -5,9 +5,9 @@ require 'eac_ruby_utils/patches/object/if_present'
 RSpec.describe ::Object do
   describe '#if_present' do
     it { expect(nil.if_present).to eq(nil) }
-    it { expect(nil.if_present('default')) .to eq('default') }
+    it { expect(nil.if_present('default')).to eq('default') }
     it { expect(nil.if_present('default') { |_v| 'calculated' }).to eq('default') }
     it { expect('present'.if_present('default')).to eq('present') }
-    it { expect('present'.if_present { |v| v + '_calculated' }) .to eq('present_calculated') }
+    it { expect('present'.if_present { |v| v + '_calculated' }).to eq('present_calculated') }
   end
 end
