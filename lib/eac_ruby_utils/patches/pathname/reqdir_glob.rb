7a3b173382fb68fc2d353514ce3dabe925843d11
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-20 22:55:11 -0300

    .rubocop.yml#Style/RedundantException: remove.

--- lib/eac_ruby_utils/patches/pathname/reqdir_glob.rb
+++ lib/eac_ruby_utils/patches/pathname/reqdir_glob.rb
@@ -6,7 +6,7 @@ class Pathname
   # A .glob that raises a ::RuntimeError if +self+ is not a directory.
   # @return [Pathname]
   def reqdir_glob(*args)
-    raise ::RuntimeError, "\"#{self}\" is not a directory" unless directory?
+    raise "\"#{self}\" is not a directory" unless directory?
 
     glob(*args)
   end
