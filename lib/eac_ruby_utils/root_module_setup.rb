6b829d638ef0dbdeff8de3bd3808a274f93115a2
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-04-29 01:52:22 -0300

    EacRubyUtils::RootModuleSetup.ignore.

--- lib/eac_ruby_utils/root_module_setup.rb
+++ lib/eac_ruby_utils/root_module_setup.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'eac_ruby_utils/patches/pathname'
+require 'eac_ruby_utils/patches/object/to_pathname'
 require 'zeitwerk'
 
 module EacRubyUtils
@@ -17,6 +19,20 @@ module EacRubyUtils
     def initialize(root_module_file, &block)
       self.root_module_file = root_module_file
       self.block = block
+    end
+
+    # @param path [String] Relative path to root module's directory.
+    def ignore(path)
+      count_before = loader.send(:ignored_paths).count
+      target_path = path.to_pathname.basename_sub { |b| "#{b.basename('.*')}.rb" }
+                      .expand_path(root_module_directory)
+      result = loader.ignore target_path
+      return result if result.count > count_before
+
+      raise ::ArgumentError, [
+        "Trying to ignore path \"#{path}\" did not increase the ignored paths.",
+        "Argument path: \"#{path}\"", "Target path: \"#{target_path}\"", "Ignored paths: #{result}"
+      ].join("\n")
     end
 
     # @return [void]
