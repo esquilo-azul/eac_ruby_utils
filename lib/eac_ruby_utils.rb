963a8ccd96c46a9881b8cace7ae740fdf9e27046
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-04-29 01:52:23 -0300

    EacRubyUtils: ignore zeitwerk non-regular files.

--- lib/eac_ruby_utils.rb
+++ lib/eac_ruby_utils.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/root_module_setup'
-EacRubyUtils::RootModuleSetup.perform __FILE__
+EacRubyUtils::RootModuleSetup.perform __FILE__ do
+  ignore 'core_ext'
+  ignore 'locales/from_all_gems'
+  ignore 'patches'
+  ignore 'ruby/on_clean_environment'
+end
 
 module EacRubyUtils
 end
