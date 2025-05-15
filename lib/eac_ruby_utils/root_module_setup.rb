6f0a7a0fe44a5372b83514f3665e7f3fcdb3c106
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-05-15 12:01:02 -0300

    EacRubyUtils::RootModuleSetup.initialize.

--- lib/eac_ruby_utils/root_module_setup.rb
+++ lib/eac_ruby_utils/root_module_setup.rb
@@ -8,9 +8,21 @@ module EacRubyUtils
       def perform; end
     end
 
+    attr_reader :root_module_file
+
+    # @param root_module_file [String]
+    def initialize(root_module_file)
+      self.root_module_file = root_module_file
+    end
+
+    # @return [void]
     def perform
       loader = Zeitwerk::Loader.for_gem
       loader.setup
     end
+
+    protected
+
+    attr_writer :root_module_file
   end
 end
