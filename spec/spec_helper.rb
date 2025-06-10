0e868a41ca9b6649a4f869a343e6cbad59e0b62a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-06-10 10:37:53 -0300

    Ruby#spec/spec_helper: correção temporária.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,5 @@ I18n.locale = 'pt-BR' # rubocop:disable Rails/I18nLocaleAssignment
 require 'eac_ruby_utils'
 require 'eac_ruby_gem_support'
 require 'avm/eac_ubuntu_base0'
+require 'avm/rspec/setup/launcher'
 EacRubyUtils::Rspec.default_setup_create(File.expand_path('..', __dir__))
