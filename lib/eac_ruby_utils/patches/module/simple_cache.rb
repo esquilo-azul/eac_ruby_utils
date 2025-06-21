3046406176d637a06a054137156ff33a7b8786c1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-06-21 10:25:50 -0300

    Module.enable_simple_cache: fix.

--- lib/eac_ruby_utils/patches/module/simple_cache.rb
+++ lib/eac_ruby_utils/patches/module/simple_cache.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/patch_module'
+require 'eac_ruby_utils/patches/module/patch_self'
 require 'eac_ruby_utils/simple_cache'
 
 class Module
   def enable_simple_cache
-    ::EacRubyUtils.patch_module(self, ::EacRubyUtils::SimpleCache)
+    patch_self(::EacRubyUtils::SimpleCache)
   end
 end
