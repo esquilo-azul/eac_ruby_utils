6a4dc67deea1f9864387658bbb5f961c48bae7fe
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-06-21 10:27:23 -0300

    Module.patch_self: fix.

--- lib/eac_ruby_utils/patches/module/patch_self.rb
+++ lib/eac_ruby_utils/patches/module/patch_self.rb
@@ -4,6 +4,6 @@ require 'eac_ruby_utils/patch_module'
 
 class Module
   def patch_self(patch_module)
-    ::EacRubyUtils.patch_module(self, patch_module)
+    ::EacRubyUtils::PatchModule.patch_module(self, patch_module)
   end
 end
